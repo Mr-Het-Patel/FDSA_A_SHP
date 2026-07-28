f4ceb34b7f321c10cde0f85d7fc948a09a97feef
Mr-Het-Patel <mail2hetpatel@gmail.com>
2026-07-28 22:21:37 +0530

    Create P2/p_2.2.cpp - Binary search program

---
+++ P2/p_2.2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int start=0;
+    int end=9;
+    int x;
+    cin>>x;
+    while(start<=end){
+        int mid=(start +end)/2;
+    if(arr[mid]==x){
+        cout<<"element found at:"<<start+1<<endl;
+        break;
+    }
+    else if(arr[mid]>x){
+        end=mid-1;
+    }
+    else if(arr[mid]<x){
+        start=mid+1;
+    }
+}
+}
